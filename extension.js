189f6b242668df0533b47eca26e15bc0692fa02e
jecrs687 <emanuelcascone@gmail.com>
2020-05-23 13:14:47 -0300

    update
    organização de codigo

--- extension.js
+++ extension.js
@@ -1,59 +1,86 @@
 
+async function buttonVelocidade(header){
+    const button = document.createElement('button')
+    button.innerHTML = "1.5x";
+    button.classList.add("buttonVel");
+    button.ondblclick = () => {
+        
+    }
+    header.appendChild(button)
+    var pressionado = false
+    window.addEventListener("click",()=>{
+        const audios = document.querySelectorAll("audio")
 
+        if(pressionado)
+        {
+            audios.forEach((audio)=>{
+                audio.playbackRate = 1.5
+            })
+        }else{     
+            audios.forEach((audio)=>{
+            audio.playbackRate = 1
+        })
+        }
+    })
+    button.addEventListener("click", ()=>{
+        if( pressionado)
+        {
+            pressionado = !pressionado
+            button.classList.remove('buttonPress');
+        }else{
+            pressionado = !pressionado
+            button.classList.add("buttonPress");            
+        }
+
+    })
+
+}
+
+async function buttonDark(header){
+    
+    var pressionado2 = false
+    const button = document.createElement('button')
+    button.innerHTML = "#";
+    button.classList.add("buttonVel");
+    header.appendChild(button)
+    button.addEventListener("click", ()=>{
+        if( pressionado2)
+        {
+            pressionado2 = !pressionado2
+            document.querySelector(":root").classList.remove("dark")
+            button.classList.remove('buttonPress');
+        }else{
+            pressionado2 = !pressionado2
+            
+            document.querySelector(":root").classList.add("dark")
+            button.classList.add("buttonPress");            
+        }
+
+    })
+
+}
+
+async function statusDownload(){
+    async function awaitEvent(){
+        console.log("deu certo")
+    }
+    const button = document.querySelector(".rAUz7").firstChild
+    button.onmouseover = ()=>{
+        document.querySelector('._3auIg').addEventListener("click", ()=>{
+            const header = document.querySelector(".zjFBX")
+            if(header){
+                awaitEvent()
+            }})
+
+    }
+}
 
 const interval = setInterval(()=>{
     const header = document.querySelector("._3auIg");
     if(header){
-        console.log(header)
         clearInterval(interval)
-        const button = document.createElement('button')
-        button.innerHTML = "1.5x";
-        button.classList.add("buttonVel");
-        header.appendChild(button)
-        var pressionado = false
-        window.addEventListener("click",()=>{
-            const audios = document.querySelectorAll("audio")
-
-            if(pressionado)
-            {
-                audios.forEach((audio)=>{
-                    audio.playbackRate = 1.5
-                })
-            }else{     
-                audios.forEach((audio)=>{
-                audio.playbackRate = 1
-            })
-            }
-        })
-        button.addEventListener("click", ()=>{
-            if( pressionado)
-            {
-                pressionado = !pressionado
-                button.classList.remove('buttonPress');
-            }else{
-                pressionado = !pressionado
-                button.classList.add("buttonPress");            
-            }
-
-        })
-        var pressionado2 = false
-        const button2 = document.createElement('button')
-        button2.innerHTML = "#";
-        button2.classList.add("buttonVel");
-        header.appendChild(button2)
-        button2.addEventListener("click", ()=>{
-            if( pressionado2)
-            {
-                pressionado2 = !pressionado2
-                document.querySelector(":root").classList.remove("dark")
-                button2.classList.remove('buttonPress');
-            }else{
-                pressionado2 = !pressionado2
-                
-                document.querySelector(":root").classList.add("dark")
-                button2.classList.add("buttonPress");            
-            }
-
-        })
+        buttonVelocidade(header);
+        buttonDark(header)
+        statusDownload()
     }
 },1000)
